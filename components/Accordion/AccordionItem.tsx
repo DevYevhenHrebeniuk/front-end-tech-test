4bdd525cbcec2029a201c8a1b1fbd8f6bbcf5b7a
DevYevhenHrebeniuk <dev.hrebeniuk@gmail.com>
2024-05-06 21:32:41 +0300

    Fixed build notes errors

--- components/Accordion/AccordionItem.tsx
+++ components/Accordion/AccordionItem.tsx
@@ -8,7 +8,7 @@ type AccordionItemProps = {
     title: string,
     description: string,
     isActive: boolean,
-    Icon: React.FC<{active?: boolean}>
+    Icon: React.FC
 }
 
 const AccordionItem = ({title, description, isActive, Icon}: AccordionItemProps) => {
@@ -22,7 +22,7 @@ const AccordionItem = ({title, description, isActive, Icon}: AccordionItemProps)
     return (
         <div className="text-white border-b border-darkGray mb-[30px] transition-all ease-in-out duration-300">
             <div className="font-SatoshiBold text-medium flex gap-[30px] items-center pr-[40px] relative cursor-pointer" onClick={handleClick}>
-                <span><Icon active={opened}/></span>
+                <span><Icon /></span>
                 {title}
                 <span className={ `${opened ? 'rotate-180': ''} w-[16px] h-[16px] absolute right-[24px] top-[50%] mt-[-8px]`}><Arrow /></span>
             </div>
